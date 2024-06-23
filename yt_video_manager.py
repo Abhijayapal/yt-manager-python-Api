f3712c90c247685684a3d742295b499bb403a9f1
Abhijayapal <palabhijaya@gmail.com>
2024-06-23 21:09:21 +0530

    modified

--- yt_video_manager.py
+++ yt_video_manager.py
@@ -2,30 +2,71 @@ import json
 
 def load_data():
     try:
-        with open('yt_manager','r') as file:
+        with open('yt_manager.txt','r') as file:
+        # giving us data as string 
             return json.load(file)
     except FileNotFoundError:
         return []
-    
+
+# helper data-situations can be directly handled
+def save_data_helper(videos):
+    with open('yt_manager.txt','w') as file:
+        # dump-inserting data- 2 parameters
+        # what to be written, where to be written
+        json.dump(videos,file)
+
 def list_all_vdo(videos):
-    pass
+    # enumerate add indexing
+    for index, video in enumerate(videos,start=1):
+        # imp statement
+        print("\n")
+        
+        print(f"{index}. {video['name']}, Duration: {video['time']}")
+                                  
+
 def update_vdo(videos):
-    pass
+    list_all_vdo(videos)
+    index=int(input("enter the video to be updated "))
+    if 1<= index <=len(videos):
+        name=input("enter new video name")
+        time=input("enter new video time")
+        videos[index-1]={'name':name , 'time':time}
+        save_data_helper(videos)
+    else:
+        print("invalid index selected")
+
+
+
 def add_vdo(videos):
-    pass
+    # 2 input to be taken from user
+    name=input("enter video name")
+    time=input("enter video time")
+    videos.append({'name':name, 'time':time})
+    save_data_helper(videos)
+    
 def delete_vdo(videos):
-    pass
+    list_all_vdo(videos)
+    index=int(input("enter the video to be deleted "))
+    if 1<= index <=len(videos):
+        
+        del videos[index-1]
+
+        save_data_helper(videos)
+    else:
+        print("invalid index selected")
+    
 
 def main():
     videos=load_data()
     while True:
         print("\n Youtube manager")
-        print("1. list of favourite videos")
-        print("2. Add a youtube videos")
-        print("3. Update a youtube videos")
-        print("4. delete a youtube videos")
-        print("5. Exit")
-        choice=input("enter your choice")
+        print("1. list of favourite videos ")
+        print("2. Add a youtube videos ")
+        print("3. Update a youtube videos ")
+        print("4. delete a youtube videos ")
+        print("5. Exit ")
+        choice=input("enter your choice ")
+        # print(videos)
 
         # match syntax
         match choice:
@@ -40,7 +81,7 @@ def main():
             case '5':
                 break
             case _:
-                print("invalid choice")
+                print("invalid choice ")
 
 if __name__=="__main__":
     main()
